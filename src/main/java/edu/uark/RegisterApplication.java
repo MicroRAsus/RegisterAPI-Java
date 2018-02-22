9ceaa5e60bcf26a6b9ced61db57ab7bcacbd300a
Lyru <35673483+Lyru@users.noreply.github.com>
2018-02-22 06:44:08 -0600

    Fixing merge

--- src/main/java/edu/uark/RegisterApplication.java
+++ src/main/java/edu/uark/RegisterApplication.java
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-package edu.uark;
-//Hi there everyone!!
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class RegisterApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(RegisterApplication.class, args);
-	}
-}
-=======
 package edu.uark;
 //Alicia's test comment V:3.0
 //Austin Brown test comment
@@ -27,4 +13,4 @@ public class RegisterApplication {
 		SpringApplication.run(RegisterApplication.class, args);
 	}
 }
->>>>>>> 4b896ffea588cfe8bbeb089511559c5dcefcb7cc
+
