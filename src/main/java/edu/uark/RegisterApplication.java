41f6936bef765d2cea6c8d19305e34357bb175ef
Jim <linchenglong12@gmail.com>
2018-02-25 23:23:50 -0600

    reverse

--- src/main/java/edu/uark/RegisterApplication.java
+++ src/main/java/edu/uark/RegisterApplication.java
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 package edu.uark;
-//Hi there everyone!!
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,20 +10,3 @@ public class RegisterApplication {
 		SpringApplication.run(RegisterApplication.class, args);
 	}
 }
-=======
-package edu.uark;
-//Alicia's test comment V:3.0
-//Austin Brown test comment
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-//Cole's Test Comment
-
-@SpringBootApplication
-public class RegisterApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(RegisterApplication.class, args);
-	}
-}
->>>>>>> 4b896ffea588cfe8bbeb089511559c5dcefcb7cc
